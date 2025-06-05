3bcf49e5610160efd1618da49687b0e63737fdbd
cfkubo <arul.vannala@gmail.com>
2025-06-05 11:19:33 -0600

    home

---
+++ src/main/java/com/example/gemfireapi/controller/WelcomeController.java
@@ -0,0 +1,21 @@
+package com.example.gemfireapi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/")
+    public ResponseEntity<String> welcome() {
+        String msg = """
+            <h2>Welcome to the Spring Boot GemFire API!</h2>
+            <ul>
+                <li>PUT data: <code>PUT /api/put?key=yourKey&amp;value=yourValue</code></li>
+                <li>GET data: <code>GET /api/get?key=yourKey</code></li>
+            </ul>
+            <p>Swagger UI: <a href="/swagger-ui.html">/swagger-ui.html</a></p>
+            """;
+        return ResponseEntity.ok().header("Content-Type", "text/html").body(msg);
+    }
+}
